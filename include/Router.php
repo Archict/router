45fb40823abbc8fe17a5ba1a8c304d0808813a54
Kevin Traini <kevin@ktraini.com>
2024-04-25 18:54:20 +0200

    feat: Introduce Router Service
    Part of #7

---
+++ include/Router.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Archict\Router;
+
+use Archict\Brick\Service;
+
+#[Service]
+final readonly class Router
+{
+}
---
+++ tests/unit/RouterTest.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Archict\Router;
+
+use Archict\Core\Core;
+use Archict\Core\Services\ServiceManager;
+use PHPUnit\Framework\TestCase;
+
+final class RouterTest extends TestCase
+{
+    private ServiceManager $service_manager;
+
+    protected function setUp(): void
+    {
+        $core = Core::build();
+        $core->load();
+        $this->service_manager = $core->service_manager;
+    }
+
+    public function testRouterIsLoaded(): void
+    {
+        self::assertTrue($this->service_manager->has(Router::class));
+        self::assertInstanceOf(Router::class, $this->service_manager->get(Router::class));
+    }
+}
